13d8b4f8658fcf14e57fbaa3735045aa9fc46234
davincecode <hyperscript.ca@gmail.com>
2022-02-23 21:11:54 +0000

    fixed broken url/new link

--- express_server.js
+++ express_server.js
@@ -43,7 +43,7 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
   };
-  res.render("urls_new");
+  res.render("urls_new", templateVars);
 });
 
 /* generated shortURL page */
