77528ae79af405919ecc8892ad79535713b12b0c
davincecode <hyperscript.ca@gmail.com>
2022-02-23 22:59:57 +0000

    added registration functionality on server

--- express_server.js
+++ express_server.js
@@ -35,8 +35,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* Routes */
 
 /* homepage */
+const fetchUserInfo = (userDB, email) => {
+  let userInfo = undefined;
+
+  if (userDB[email]) {
+    userInfo = userDB[email];
+  } else {
+    userInfo = {};
+  }
+};
+
+const authenticateUser = (userDB, email, password) => {
+  if (!userDB[email]) {
+    return { error: "bad email", data: null };
+  }
+
+  if (userDB[email].password !== password) {
+    return { error: "bad password", data: null };
+  }
+
+  return { error: null, data: userDB[email] };
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const userInfo = fetchUserInfo(users, req.cookies.email);
+  const templateVars = { username: username };
+  return res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -101,7 +125,7 @@ app.post("/urls/:shortURL/update", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-/* Register */
+/* register get */
 app.get("/register", (req, res) => {
   const templateVars = {
     username: req.cookies["username"],
@@ -109,9 +133,53 @@ app.get("/register", (req, res) => {
   res.render("register", templateVars);
 });
 
+const createUser = (userDB, userInfo) => {
+  const { email, password } = userInfo;
+
+  if (!email || !password) {
+    return { error: "One of the fields is invalid", data: null };
+  }
+
+  if (users[email]) {
+    return { error: "account already exist", data: null };
+  }
+  const newUser = { email, password };
+  userDB[email] = newUser;
+
+  return { error: null, data: newUser };
+};
+
+/* register post */
+app.post("/register", (req, res) => {
+  const { error, data } = createUser(users, req.body);
+
+  if (error) {
+    console.log(error);
+    return res.redirect("/register");
+  }
+  res.cookie("email", email);
+
+  return res.redirect("/urls");
+});
+
 /* Login */
 app.post("/login", (req, res) => {
-  const username = req.body.username;
+  // const username = req.body.username;
+  const { email, password } = req.body;
+
+  const { error, data } = authenticateUser(users, email, password);
+
+  if (!users[email]) {
+    console.log("bad email");
+    return res.redirect("/");
+
+    if (users[email].password === password) {
+      console.log("bad password");
+      return res.redirect("/");
+    }
+    return res.redirect("/");
+  }
+
   res.cookie("username", username);
   res.redirect(`/urls`);
 });
