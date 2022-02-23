6c27f42e72c04d1f620e568172a3083a7487fa38
davincecode <hyperscript.ca@gmail.com>
2022-02-23 21:39:41 +0000

    added users

--- express_server.js
+++ express_server.js
@@ -11,6 +11,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 const generateRandomStr = () => (Math.random() + 1).toString(36).substring(7);
 
 /* middleware */
